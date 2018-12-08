d7717f7c8496ec8e352f078777d93799438bd7ff
Leo <leonardo.f.jr@gmail.com>
2018-12-08 00:06:25 -0500

    updated

--- resources/views/frontend/components/jumbotron.blade.php
+++ resources/views/frontend/components/jumbotron.blade.php
@@ -1,7 +1,7 @@
 <div class="jumbotron jumbotron-fluid">
     <div>
         <div style="position: absolute; top: 50%; left: 50%; color: #fff; transform: translate(-50%, -50%)">
-          <h1 class="main-heading">Your new logo will say a lot about who you are – visually, viscerally and emotionally.</h1>
+          <h1 class="main-heading">Multi-page is a thing of the past, Get a bigger return on your investment with Single Page Applications.</h1>
         </div>
     </div>
 </div>
