e5a60036009adff9902c055f4255751610af5c87
Leo <leonardo.f.jr@gmail.com>
2019-01-01 01:27:31 -0500

    /

--- resources/views/frontend/components/partials/head.blade.php
+++ resources/views/frontend/components/partials/head.blade.php
@@ -3,7 +3,7 @@
 <script src="https://cdn.rawgit.com/scottjehl/picturefill/3.0.2/dist/picturefill.min.js"></script>
 <title>Beauty Bliss - @yield('title')</title>
 <meta name="viewport" content="width=device-width, initial scale=1.0">
-<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400|Open+Sans:300,400|Oswald:200,300,400|Roboto:100,300,400|Vollkorn" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" href= "/css/slick-1.8.0/slick.css">
