5a9f725580d07cc1c46d4de60b42a77658f7b1d6
Leo <leonardo.f.jr@gmail.com>
2019-01-07 07:02:52 -0500

    /

--- resources/views/frontend/pages/portfolio.blade.php
+++ resources/views/frontend/pages/portfolio.blade.php
@@ -9,14 +9,13 @@
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
-                <div class="modal-body">
-                    <img class="img-fluid">
-                </div>
                 <div class="modal-header">
-                    <h5 class="modal-description" id="exampleModalLabel"></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
+                </div>
+                <div class="modal-body text-center">
+                    <img class="img-fluid">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
